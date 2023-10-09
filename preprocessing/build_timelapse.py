05bd6ecf0591f0dfac2cb868ccc6ca9c1ee9d437
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2023-10-09 11:53:44 -0500

    FIX: Adjusted timelapse for P5 and P6

--- preprocessing/build_timelapse.py
+++ preprocessing/build_timelapse.py
@@ -14,7 +14,7 @@ with open("../config.yaml", "r") as f:
 
 path_audio = config["input_data"]["path_audio"]
 path_metadata = config["preprocessing"]["path_save_metadata_t1"]
-sample_len = 1
+sample_len = 10
 site_sel = 'Z3-G094'
 
 #%% Sample audio for overall examination - timelapse soundscapes
@@ -31,3 +31,16 @@ for day, df_site in df_timelapse.groupby('day'):
                                 display=False)
     long_wav = sound.resample(long_wav, fs, target_fs=48000)
     sound.write('../../output/figures/'+site_sel+'_'+day+'_timelapse.wav', 48000, long_wav, bit_depth=16)
+
+#%% Make adjustments for sites P5 and P6
+sample_len = 10
+df_timelapse = pd.read_csv('../../output/metadata/metadata_t2_P6_05-31_06-06.csv')
+len(df_timelapse)
+pd.to_datetime(df_timelapse.date).dt.day.value_counts()
+
+# create time lapse
+long_wav, fs = concat_audio(df_timelapse['path_audio'],
+                            sample_len=sample_len, 
+                            verbose=True,
+                            display=False)
+sound.write('/Volumes/MANGO/P6_lastday_timelapse.wav', fs, long_wav, bit_depth=16)
